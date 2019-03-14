654cf269b7ac56335654e37a9f61c2dd15af5b2c
Ammaryasser <ammaryasser085@gmail.com>
2019-03-14 17:37:42 +0200

    add DIO_WritePort function

--- Project/DIO.c
+++ Project/DIO.c
@@ -1,6 +1,4 @@
 #include "DIO.h"
-#include "typedefs.h"
-#include "tm4c123gh6pm.h"
 
 //reads a pin and returns 1 or 0
 uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask)
@@ -69,3 +67,41 @@ Dio_LevelType DIO_ReadPort_level(uint8 port_index, uint8 pins_mask)
 			return 0;;
 	}
 }
+
+//write 0 or 1 on specific pin in PORT_X 
+void DIO_WritePort(uint8 port_index, uint8 pins_mask, Dio_LevelType pins_level)
+	{
+			switch(port_index)
+		{
+			case 0:
+				if(pins_level == STD_HIGH)
+						GPIO_PORTA_DATA_R |= pins_mask;
+				else 
+						GPIO_PORTA_DATA_R &= ~pins_mask;
+			case 1:
+				if(pins_level == STD_HIGH)
+						GPIO_PORTB_DATA_R |= pins_mask;
+				else 
+						GPIO_PORTB_DATA_R &= ~pins_mask;
+			case 2:
+				if(pins_level == STD_HIGH)
+						GPIO_PORTC_DATA_R |= pins_mask;
+				else 
+						GPIO_PORTC_DATA_R &= ~pins_mask;
+			case 3:
+				if(pins_level == STD_HIGH)
+						GPIO_PORTD_DATA_R |= pins_mask;
+				else 
+						GPIO_PORTD_DATA_R &= ~pins_mask;
+			case 4:
+				if(pins_level == STD_HIGH)
+						GPIO_PORTE_DATA_R |= pins_mask;
+				else 
+						GPIO_PORTE_DATA_R &= ~pins_mask;
+			case 5:
+				if(pins_level == STD_HIGH)
+						GPIO_PORTF_DATA_R |= pins_mask;
+				else 
+						GPIO_PORTF_DATA_R &= ~pins_mask;
+			}
+	}
