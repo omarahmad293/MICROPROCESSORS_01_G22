3531aa0a169f8ca93260c61052a58dfc2cf59215
Unknown <lameesemad1996@hotmail.com>
2019-03-14 07:45:30 +0300

    removed warnings

--- Project/DIO.c
+++ Project/DIO.c
@@ -11,32 +11,26 @@ uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask)
 				if((GPIO_PORTA_DATA_R&pins_mask) == 0)
 					return 0;
 				else return 1;
-				break;
 		case 1:
 				if((GPIO_PORTB_DATA_R&pins_mask) == 0)
 					return 0;
 				else return 1;
-				break;
 		case 2:
 				if((GPIO_PORTC_DATA_R&pins_mask) == 0)
 					return 0;
 				else return 1;
-				break;
 		case 3:
 				if((GPIO_PORTD_DATA_R&pins_mask) == 0)
 					return 0;
 				else return 1;
-				break;
 		case 4:
 				if((GPIO_PORTE_DATA_R&pins_mask) == 0)
 					return 0;
 				else return 1;
-				break;
 		case 5:
 				if((GPIO_PORTF_DATA_R&pins_mask) == 0)
 					return 0;
 				else return 1;
-				break;
 		default:
 			return 0;
 	}
@@ -51,33 +45,27 @@ Dio_LevelType DIO_ReadPort_level(uint8 port_index, uint8 pins_mask)
 				if((GPIO_PORTA_DATA_R&pins_mask) == 0)
 					return STD_LOW;
 				else return STD_HIGH;
-				break;
 		case 1:
 				if((GPIO_PORTB_DATA_R&pins_mask) == 0)
 					return STD_LOW;
 				else return STD_HIGH;
-				break;
 		case 2:
 				if((GPIO_PORTC_DATA_R&pins_mask) == 0)
 					return STD_LOW;
 				else return STD_HIGH;
-				break;
 		case 3:
 				if((GPIO_PORTD_DATA_R&pins_mask) == 0)
 					return STD_LOW;
 				else return STD_HIGH;
-				break;
 		case 4:
 				if((GPIO_PORTE_DATA_R&pins_mask) == 0)
 					return STD_LOW;
 				else return STD_HIGH;
-				break;
 		case 5:
 				if((GPIO_PORTF_DATA_R&pins_mask) == 0)
 					return STD_LOW;
 				else return STD_HIGH;
-				break;
 		default:
-			return 0;
+			return 0;;
 	}
 }
--- Project/dio.d
+++ Project/dio.d
@@ -1,3 +1,4 @@
 .\dio.o: DIO.c
 .\dio.o: DIO.h
 .\dio.o: typedefs.h
+.\dio.o: tm4c123gh6pm.h
