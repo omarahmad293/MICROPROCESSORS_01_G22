4bf77de375de18bca5630ec5ef011f81a6620686
Unknown <lameesemad1996@hotmail.com>
2019-03-14 07:38:36 +0300

    defined pin masks and implemented two DIO_Read_Port functions
    Implemented Fn Headers:
    uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask);
    Dio_LevelType DIO_ReadPort_level(uint8 port_index, uint8 pins_mask);

--- Project/DIO.c
+++ Project/DIO.c
@@ -1,5 +1,83 @@
 #include "DIO.h"
 #include "typedefs.h"
+#include "tm4c123gh6pm.h"
 
+//reads a pin and returns 1 or 0
+uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask)
+{
+	switch(port_index)
+	{
+		case 0:
+				if((GPIO_PORTA_DATA_R&pins_mask) == 0)
+					return 0;
+				else return 1;
+				break;
+		case 1:
+				if((GPIO_PORTB_DATA_R&pins_mask) == 0)
+					return 0;
+				else return 1;
+				break;
+		case 2:
+				if((GPIO_PORTC_DATA_R&pins_mask) == 0)
+					return 0;
+				else return 1;
+				break;
+		case 3:
+				if((GPIO_PORTD_DATA_R&pins_mask) == 0)
+					return 0;
+				else return 1;
+				break;
+		case 4:
+				if((GPIO_PORTE_DATA_R&pins_mask) == 0)
+					return 0;
+				else return 1;
+				break;
+		case 5:
+				if((GPIO_PORTF_DATA_R&pins_mask) == 0)
+					return 0;
+				else return 1;
+				break;
+		default:
+			return 0;
+	}
+}
 
-
+//extra function that reads a pin and returns level type
+Dio_LevelType DIO_ReadPort_level(uint8 port_index, uint8 pins_mask)
+{
+	switch(port_index)
+	{
+		case 0:
+				if((GPIO_PORTA_DATA_R&pins_mask) == 0)
+					return STD_LOW;
+				else return STD_HIGH;
+				break;
+		case 1:
+				if((GPIO_PORTB_DATA_R&pins_mask) == 0)
+					return STD_LOW;
+				else return STD_HIGH;
+				break;
+		case 2:
+				if((GPIO_PORTC_DATA_R&pins_mask) == 0)
+					return STD_LOW;
+				else return STD_HIGH;
+				break;
+		case 3:
+				if((GPIO_PORTD_DATA_R&pins_mask) == 0)
+					return STD_LOW;
+				else return STD_HIGH;
+				break;
+		case 4:
+				if((GPIO_PORTE_DATA_R&pins_mask) == 0)
+					return STD_LOW;
+				else return STD_HIGH;
+				break;
+		case 5:
+				if((GPIO_PORTF_DATA_R&pins_mask) == 0)
+					return STD_LOW;
+				else return STD_HIGH;
+				break;
+		default:
+			return 0;
+	}
+}
--- Project/DIO.h
+++ Project/DIO.h
@@ -3,10 +3,20 @@
 
 #include "typedefs.h"
 
+//define pin masks 
+#define MASK_0	0b00000001
+#define MASK_1	0b00000010
+#define MASK_2	0b00000100
+#define MASK_3	0b00001000
+#define MASK_4	0b00010000
+#define MASK_5	0b00100000
+#define MASK_6	0b01000000
+#define MASK_7	010000000
+
 typedef enum {STD_LOW = 0, STD_HIGH = 1} Dio_LevelType; 
 
-
 uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask);
+Dio_LevelType DIO_ReadPort_level(uint8 port_index, uint8 pins_mask);
 void DIO_WritePort(uint8 port_index, uint8 pins_mask, Dio_LevelType pins_level);
 void DIO_FlipPort(uint8 port_index, uint8 pins_mask);
 
