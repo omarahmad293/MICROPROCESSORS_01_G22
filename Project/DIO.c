c9f9b95aea27f63ddc6c5ea927ffe24ab5c646e5
Omar Ibrahim <36420865+mr-rofl@users.noreply.github.com>
2019-03-15 00:10:42 +0200

    added toggle

--- Project/DIO.c
+++ Project/DIO.c
@@ -105,3 +105,22 @@ void DIO_WritePort(uint8 port_index, uint8 pins_mask, Dio_LevelType pins_level)
 						GPIO_PORTF_DATA_R &= ~pins_mask;
 			}
 	}
+	//toggles pins for port_x
+void DIO_FlipPort(uint8 port_index, uint8 pins_mask){
+	switch (port_index){
+		case 0:
+			GPIO_PORTA_DATA_R  ^= pins_mask;
+		case 1:
+			GPIO_PORTB_DATA_R  ^= pins_mask;
+		case 2:
+			GPIO_PORTC_DATA_R  ^= pins_mask;
+		case 3:
+			GPIO_PORTD_DATA_R  ^= pins_mask;
+		case 4:
+			GPIO_PORTE_DATA_R  ^= pins_mask;
+		case 5:
+			GPIO_PORTF_DATA_R  ^= pins_mask;
+	
+	}
+
+}
