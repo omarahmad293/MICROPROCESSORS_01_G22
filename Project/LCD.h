bc37e92c7029539a741ab60ae72fcabea4cb2f55
Matt <matttppp@gmail.com>
2019-03-18 00:20:55 +0200

    Fixing conflic

--- Project/LCD.h
+++ Project/LCD.h
@@ -3,11 +3,8 @@
 
 #include "typedefs.h"
 #include "tm4c123gh6pm.h"
-<<<<<<< HEAD
 #include "DIO.h"
-=======
 #include "timer.h"
->>>>>>> 917780e97c8459cea564c8c4261751b70b73a1fc
 
 void LCD_sendCommand(uint8 command);
 void LCD_sendInt(uint8 number);
