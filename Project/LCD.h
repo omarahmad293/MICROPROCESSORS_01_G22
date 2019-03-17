917780e97c8459cea564c8c4261751b70b73a1fc
Omar Ibrahim <36420865+mr-rofl@users.noreply.github.com>
2019-03-18 00:06:48 +0200

    added send_int

--- Project/LCD.h
+++ Project/LCD.h
@@ -3,10 +3,10 @@
 
 #include "typedefs.h"
 #include "tm4c123gh6pm.h"
+#include "timer.h"
 
 void LCD_sendCommand(uint8 command);
 void LCD_sendInt(uint8 number);
 void LCD_init(void);
 void LCD_clearScreen(void);
-
 #endif
